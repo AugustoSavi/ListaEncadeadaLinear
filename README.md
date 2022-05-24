7e5c609350c1184cddd35cbef9f7b20f3309c573
augusto.savi <augusto.savi@betha.com.br>
2022-05-23 21:14:33 -0300

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Lista_encadeada_linear
+# Lista Encadeada Linear
+
+# Projeto Aviões
+    Projeto proposto pelo professor Giácomo Antônio Althoff Bolan, tem como propósito, usar a matemática e a computação gráfica para proteger vidas de possíveis acidentes aéreos. 
+
+![Badge](https://img.shields.io/github/issues/AugustoSavi/ListaEncadeadaLinear)
+![Badge](https://img.shields.io/github/forks/AugustoSavi/ListaEncadeadaLinear)
+![Badge](https://img.shields.io/github/stars/AugustoSavi/ListaEncadeadaLinear)
+![Badge](https://img.shields.io/github/license/AugustoSavi/ListaEncadeadaLinear)
+![Badge](https://img.shields.io/twitter/url?url=https%3A%2F%2Fgithub.com%2FAugustoSavi%2FListaEncadeadaLinear)
+
+![image](https://user-images.githubusercontent.com/32443720/169923716-e3c38ad1-8274-4dad-b56c-d469c87ad6ee.png)
+
+## Pré-requisitos
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+[Git](https://git-scm.com)
+Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
+
+## 🎲 Rodando a aplicação
+
+```bash
+# Clone este repositório
+$ git clone https://github.com/AugustoSavi/ListaEncadeadaLinear
+
+# Acesse a pasta do projeto no terminal/cmd
+$ cd ListaEncadeadaLinear
+
+# compile e execute o arquivo main.ccp da forma que quiser
+```
+
+## 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- C++
--- main.cpp
+++ main.cpp
@@ -1,225 +1,257 @@
 #include <iostream>
 #include <cstdlib>
 
-//define o tamanho da lista, mais este tamanho poderia ser passado por parâmetro para o método criar lista
+// define o tamanho da lista, mais este tamanho poderia ser passado por parï¿½metro para o mï¿½todo criar lista
 #define TAM 10
 
 using namespace std;
 
-
-struct lista{
-   int ult;           //possui a posição do último elemento da lista
-   int valor[TAM];     //cria uma lista de inteiros com o número de elementos definidos com #define TAM
+struct lista
+{
+     int ult;        // possui a posiï¿½ï¿½o do ï¿½ltimo elemento da lista
+     int valor[TAM]; // cria uma lista de inteiros com o nï¿½mero de elementos definidos com #define TAM
 };
 
-//1 A)primeiro método a ser executado, cria a lista com o valor -1 
-void criaLista(lista *lis){
+// 1 A)primeiro mï¿½todo a ser executado, cria a lista com o valor -1
+void criaLista(lista *lis)
+{
      lis->ult = -1;
 }
 
-//1 B)verifica se a lista está vazia
-bool listaVazia(lista *lis){
-   if(lis->ult == -1){
-      return true;
-   }
-   return false;
-}
-
-//1 C)verifica se a lista está cheia
-bool listaCheia(lista *lis){
-     if(lis->ult == TAM -1){
-       return true;
+// 1 B)verifica se a lista estï¿½ vazia
+bool listaVazia(lista *lis)
+{
+     if (lis->ult == -1)
+     {
+          return true;
      }
      return false;
 }
 
-//1 D)insere o elemento passado por parâmetro no final da lista
-void insereFinal(lista *lis, int valorIns){
-     if(listaCheia(lis)){
-       cout << "Nao é possivel inserir no final pois a lista esta cheia." << endl;
-       return;
+// 1 C)verifica se a lista estï¿½ cheia
+bool listaCheia(lista *lis)
+{
+     if (lis->ult == TAM - 1)
+     {
+          return true;
      }
-     //incrementa 1 à variável que mantém o valor do último elemento
+     return false;
+}
+
+// 1 D)insere o elemento passado por parï¿½metro no final da lista
+void insereFinal(lista *lis, int valorIns)
+{
+     if (listaCheia(lis))
+     {
+          cout << "Nao ï¿½ possivel inserir no final pois a lista esta cheia." << endl;
+          return;
+     }
+     // incrementa 1 ï¿½ variï¿½vel que mantï¿½m o valor do ï¿½ltimo elemento
      lis->ult++;
-     //insere o elemento no final da lista
-     lis->valor[ lis->ult ] = valorIns;
+     // insere o elemento no final da lista
+     lis->valor[lis->ult] = valorIns;
 }
 
-//1 E)remove um elemento do final
-void removeFinal(lista *lis){
-    if(listaVazia(lis)){
-       cout << "Nao é possivel remover um elemento do final pois a lista esta vazia." << endl;
-       return;
-    }
+// 1 E)remove um elemento do final
+void removeFinal(lista *lis)
+{
+     if (listaVazia(lis))
+     {
+          cout << "Nao Ã© possivel remover um elemento do final pois a lista esta vazia." << endl;
+          return;
+     }
 
-   //apenas decrementando 1 ao último elemento não será mais possível acessar esta posição
-   lis->ult--;
+     // apenas decrementando 1 ao ï¿½ltimo elemento nï¿½o serï¿½ mais possï¿½vel acessar esta posiï¿½ï¿½o
+     lis->ult--;
 }
 
-//1 F)consulta todos os elemento da lista, apresentando os valores inseridos
-void consultaLista(lista *lis){
-    if(listaVazia(lis)){
-       cout << "Nao é possivel consultar pois a lista esta vazia." << endl;
-       return;
-    }
-    
-    //percorre todos os elementos da lista a partir do início apresentando os valores inseridos
-    for(int i = 0; i <= lis->ult; i++){
-       cout << "Valor [" << i << "]: " << lis->valor[i] << endl;
-    }  
+// 1 F)consulta todos os elemento da lista, apresentando os valores inseridos
+void consultaLista(lista *lis)
+{
+     if (listaVazia(lis))
+     {
+          cout << "Nao ï¿½ possivel consultar pois a lista esta vazia." << endl;
+          return;
+     }
+
+     // percorre todos os elementos da lista a partir do inï¿½cio apresentando os valores inseridos
+     for (int i = 0; i <= lis->ult; i++)
+     {
+          cout << "Valor [" << i << "]: " << lis->valor[i] << endl;
+     }
 }
 
-//1 G)apresenta o número de elementos inseridos na lista
-int consultaNumEleLista (lista *lis){
-	if(listaVazia(lis)){
-		cout << "A lista está vazia" << endl;
-	}
-	else
-	
-   return lis->ult;
+// 1 G)apresenta o nï¿½mero de elementos inseridos na lista
+int consultaNumEleLista(lista *lis)
+{
+     if (listaVazia(lis))
+     {
+          cout << "A lista estï¿½ vazia" << endl;
+     }
+     else
+
+          return lis->ult;
 }
 
-//1 H)retorna o ultimo elemento inserido na lista
-int consultaUltimo (lista *lis){
-	
-	if(listaVazia(lis)){
-		cout << "A lista está vazia" << endl;
-	}
-	else
-	
-   return lis->valor[lis->ult];
+// 1 H)retorna o ultimo elemento inserido na lista
+int consultaUltimo(lista *lis)
+{
+
+     if (listaVazia(lis))
+     {
+          cout << "A lista estï¿½ vazia" << endl;
+     }
+     else
+
+          return lis->valor[lis->ult];
 }
 
+// 2 A)insere o elemento passado por parï¿½metro no inicio da lista
+void insereInicio(lista *lis, int valorIns)
+{
+     if (listaVazia(lis))
+     {
+          cout << "A lista estï¿½ vazia" << endl;
+     }
+     else
+     {
 
-//2 A)insere o elemento passado por parâmetro no inicio da lista
-void insereInicio(lista *lis, int valorIns){
-	if(listaVazia(lis)){
-		cout << "A lista está vazia" << endl;
-	}
-	else{
-	
-	for(int i = lis->ult ;i<=0 ; i--){
-		lis->valor[i]=lis->valor[i+1];
-	}
-	lis->valor[0]=valorIns;
-}
-     return ;
-}
-
-//2 B)remove um elemento do início
-void removeInicio(lista *lis){
-	if(listaVazia(lis)){
-		cout << "A lista está vazia" << endl;
-	}
-	else
-	
-	for(int i=0;i<lis->ult;i++){
-		lis->valor[i]=lis->valor[i-1];
-	}
-    return ;
+          for (int i = lis->ult; i <= 0; i--)
+          {
+               lis->valor[i] = lis->valor[i + 1];
+          }
+          lis->valor[0] = valorIns;
+     }
+     return;
 }
 
-//2 C)remove uma posição do vetor
-void removePosicao(lista *lis, int pos){
-for(int i=pos;i<=lis->ult;i++){
-		lis->valor[i]=lis->valor[i+1];	
-	}
-	lis->ult--;
-   return ;
+// 2 B)remove um elemento do inï¿½cio
+void removeInicio(lista *lis)
+{
+     if (listaVazia(lis))
+     {
+          cout << "A lista estï¿½ vazia" << endl;
+     }
+     else
+
+          for (int i = 0; i < lis->ult; i++)
+          {
+               lis->valor[i] = lis->valor[i - 1];
+          }
+     return;
 }
 
-//2 D)remove um valor da lista
-void removeValor(lista *lis, int valor){
-   	for(int i=0;i<=lis->ult;i++){
-		
-		if(lis->valor[i]==valor){
-			
-			for(int j=i;j<lis->ult-1;j++){
-				
-				lis->valor[j]=lis->valor[j+1];
-				
-				lis->ult--;
-			}
-		}
-   	}
-return ;
+// 2 C)remove uma posiï¿½ï¿½o do vetor
+void removePosicao(lista *lis, int pos)
+{
+     for (int i = pos; i <= lis->ult; i++)
+     {
+          lis->valor[i] = lis->valor[i + 1];
+     }
+     lis->ult--;
+     return;
 }
 
-//2 E)inverte uma lista onde o último elemento passa a ser o primeiro e o primeiro para a ser o último
-void inverte(lista *lis){
-	int j=0,aux;
-	
-	for(int i=0;i<lis->ult;i++){
-		j=lis->ult-i-1;
-		aux=lis->valor[i];
-		lis->valor[i]=lis->valor[j];
-		lis->valor[j]=aux;
-	}
-	
-  return ;
-}
-//2 F)faz uma pesquisa sequencial, retornando a posiçaõ do elemento. Se o elemento não for encontrado, irá retornar -1
-int pesqseq(lista *lis, int valor){
-  
-  return 0;
-}
+// 2 D)remove um valor da lista
+void removeValor(lista *lis, int valor)
+{
+     for (int i = 0; i <= lis->ult; i++)
+     {
 
-//2 G) faz uma busca binária a procura do valor, retornando a posição do valor localizado no vetor
-int pesqbin(lista *lis, int valorProcurado){
+          if (lis->valor[i] == valor)
+          {
 
-   
-    return 0;
+               for (int j = i; j < lis->ult - 1; j++)
+               {
+
+                    lis->valor[j] = lis->valor[j + 1];
+
+                    lis->ult--;
+               }
+          }
+     }
+     return;
 }
 
-void iniciodalistateste(lista *listateste){
-   criaLista(listateste);
-   insereFinal(listateste,1);
-   insereFinal(listateste,12);
-   insereFinal(listateste,14);
-   insereFinal(listateste,16);
-   insereFinal(listateste,18);
-   insereFinal(listateste,21);
-   insereFinal(listateste,30);  
+// 2 E)inverte uma lista onde o ï¿½ltimo elemento passa a ser o primeiro e o primeiro para a ser o ï¿½ltimo
+void inverte(lista *lis)
+{
+     int j = 0, aux;
+
+     for (int i = 0; i < lis->ult; i++)
+     {
+          j = lis->ult - i - 1;
+          aux = lis->valor[i];
+          lis->valor[i] = lis->valor[j];
+          lis->valor[j] = aux;
+     }
+
+     return;
+}
+// 2 F)faz uma pesquisa sequencial, retornando a posiï¿½aï¿½ do elemento. Se o elemento nï¿½o for encontrado, irï¿½ retornar -1
+int pesqseq(lista *lis, int valor)
+{
+
+     return 0;
 }
 
+// 2 G) faz uma busca binï¿½ria a procura do valor, retornando a posiï¿½ï¿½o do valor localizado no vetor
+int pesqbin(lista *lis, int valorProcurado)
+{
 
-//utiliza um menu para comunicação com o usuário com o proósito de validar a utilização das funções acima declaradas
-void iniciarMenu(){
-//Teste listas Lineares
-   lista lis;
-   int valor,pos;
-   iniciodalistateste(&lis);
- 
-   int codigo = -1;
-   
- 
-   
-   while(codigo != 0){
-       system("cls"); 
-                
-       cout << "--- INFORME UMA DAS OPCOES ABAIXO ---" << endl;
-       cout << "1  - Criar" << endl;
-       cout << "2  - Inserir pelo final" << endl;
-       cout << "3  - Remover pelo final" << endl;
-       cout << "4  - Consultar todos" << endl;
-       //criar a consição a partir daqui
-       cout << "5  - Consultar numero de elementos inseridos" << endl;
-       cout << "6  - Consultar ultimo elemento" << endl;
-       cout << "7  - Inserir no inicio" << endl;
-       cout << "8  - Remover do inicio" << endl;
-       cout << "9  - Remover posicao" << endl;
-       cout << "10 - Remover valor" << endl;
-       cout << "11 - Inverter lista" << endl;
-       cout << "12 - Pesquisar valor (sequencial)" << endl;
-       cout << "13 - Pesquisar valor (binaria)" << endl;
-       cout << "14 - Ordenar lista (ASC)" << endl;
-       cout << "15 - Ordenar lista (DESC)" << endl;
-       
-       cout << "0 - Sair" << endl;
-       
-       cout << "Informe a opcao: ";
-       cin >> codigo;
-       switch(codigo){
+     return 0;
+}
+
+void iniciodalistateste(lista *listateste)
+{
+     criaLista(listateste);
+     insereFinal(listateste, 1);
+     insereFinal(listateste, 12);
+     insereFinal(listateste, 14);
+     insereFinal(listateste, 16);
+     insereFinal(listateste, 18);
+     insereFinal(listateste, 21);
+     insereFinal(listateste, 30);
+}
+
+// utiliza um menu para comunicaï¿½ï¿½o com o usuï¿½rio com o proï¿½sito de validar a utilizaï¿½ï¿½o das funï¿½ï¿½es acima declaradas
+void iniciarMenu()
+{
+     // Teste listas Lineares
+     lista lis;
+     int valor, pos;
+     iniciodalistateste(&lis);
+
+     int codigo = -1;
+
+     while (codigo != 0)
+     {
+          system("cls");
+
+          cout << "--- INFORME UMA DAS OPCOES ABAIXO ---" << endl;
+          cout << "1  - Criar" << endl;
+          cout << "2  - Inserir pelo final" << endl;
+          cout << "3  - Remover pelo final" << endl;
+          cout << "4  - Consultar todos" << endl;
+          // criar a consiï¿½ï¿½o a partir daqui
+          cout << "5  - Consultar numero de elementos inseridos" << endl;
+          cout << "6  - Consultar ultimo elemento" << endl;
+          cout << "7  - Inserir no inicio" << endl;
+          cout << "8  - Remover do inicio" << endl;
+          cout << "9  - Remover posicao" << endl;
+          cout << "10 - Remover valor" << endl;
+          cout << "11 - Inverter lista" << endl;
+          cout << "12 - Pesquisar valor (sequencial)" << endl;
+          cout << "13 - Pesquisar valor (binaria)" << endl;
+          cout << "14 - Ordenar lista (ASC)" << endl;
+          cout << "15 - Ordenar lista (DESC)" << endl;
+
+          cout << "0 - Sair" << endl;
+
+          cout << "Informe a opcao: ";
+          cin >> codigo;
+          switch (codigo)
+          {
           case 0:
                exit(0);
           case 1:
@@ -229,107 +261,113 @@ void iniciarMenu(){
 
                cout << "Informe o valor a ser inserido: ";
                cin >> valor;
-               insereFinal(&lis,valor);
+               insereFinal(&lis, valor);
                break;
           case 3:
-               
+
                removeFinal(&lis);
                break;
           case 4:
-               
+
                consultaLista(&lis);
                break;
-               
+
           case 5:
-               
-                valor = consultaNumEleLista(&lis);
-                cout << "Numero de elementos inseridos: " << valor << endl;
+
+               valor = consultaNumEleLista(&lis);
+               cout << "Numero de elementos inseridos: " << valor << endl;
                break;
           case 6:
 
-                valor = consultaUltimo(&lis);
-                cout << "O ultimo elemento inserido na lista e: " << valor << endl;
+               valor = consultaUltimo(&lis);
+               cout << "O ultimo elemento inserido na lista e: " << valor << endl;
                break;
           case 7:
-               
+
                cout << "Informe o valor a ser inserido: ";
                cin >> valor;
-               insereInicio(&lis,valor);
+               insereInicio(&lis, valor);
                break;
           case 8:
-               
+
                removeInicio(&lis);
                break;
           case 9:
-               
+
                cout << "Informe a posicao a ser removida: ";
                cin >> valor;
-               removePosicao(&lis,valor);
+               removePosicao(&lis, valor);
                break;
           case 10:
 
                cout << "Informe o valor a ser removido: ";
                cin >> valor;
-               removeValor(&lis,valor);
+               removeValor(&lis, valor);
                break;
           case 11:
-               
+
                inverte(&lis);
                break;
           case 12:
-               
+
                cout << "Informe o valor a ser pesquisado: ";
                cin >> valor;
-               pos = pesqseq(&lis,valor);
-               
-               //garante que o valor foi encontrado
-               if(pos == -1){
-                  cout << "O valor " << valor << " nao esta na lista" << endl;
-               }else{
-                  cout << "O valor " << valor << " esta na posicao " << pos << endl;
+               pos = pesqseq(&lis, valor);
+
+               // garante que o valor foi encontrado
+               if (pos == -1)
+               {
+                    cout << "O valor " << valor << " nao esta na lista" << endl;
+               }
+               else
+               {
+                    cout << "O valor " << valor << " esta na posicao " << pos << endl;
                }
                break;
           case 13:
 
                cout << "Informe o valor a ser pesquisado: ";
                cin >> valor;
-               pos = pesqbin(&lis,valor);
-               
-               //garante que o valor foi encontrado
-               if(pos == -1){
-                  cout << "O valor " << valor << " nao esta na lista" << endl;
-               }else{
-                  cout << "O valor " << valor << " esta na posicao " << pos << endl;
+               pos = pesqbin(&lis, valor);
+
+               // garante que o valor foi encontrado
+               if (pos == -1)
+               {
+                    cout << "O valor " << valor << " nao esta na lista" << endl;
+               }
+               else
+               {
+                    cout << "O valor " << valor << " esta na posicao " << pos << endl;
                }
                break;
-               
+
                break;
           case 14:
-               
-            //   ordenaAsc(&lis);
+
+               //   ordenaAsc(&lis);
                break;
           case 15:
-               
-             //  ordenaDesc(&lis);
+
+               //  ordenaDesc(&lis);
                break;
-               
+
           default:
-                  cout << "A opcao escolhida e invalida" << endl;    
-       }
-       system("pause"); 
-   }//end while
+               cout << "A opcao escolhida e invalida" << endl;
+          }
+          system("pause");
+     } // end while
 
-
-    system("pause");
+     system("pause");
 }
 
-int main(){
-    
-    //testa as funções sem a utilização do menu    
-    //testeFuncoes();
-    
-    //inicia o menu de opções para comunicação com o usuário
-    iniciarMenu();
-    
-    return 0;    
+int main()
+{
+
+     // testa as funï¿½ï¿½es sem a utilizaï¿½ï¿½o do menu
+     // testeFuncoes();
+
+     // inicia o menu de opï¿½ï¿½es para comunicaï¿½ï¿½o com o usuï¿½rio
+     iniciarMenu();
+
+     return 0;
 }
